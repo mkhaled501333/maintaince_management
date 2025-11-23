1c45d0b1a90864b31b8c0387381ef127558e8746
mkhaled501333 <mkhaled501333@gmail.com>
2025-11-23 21:46:02 +0200

    f

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    'janssencmma.cloud',
+    'https://janssencmma.cloud',
+    'http://janssencmma.cloud',
+  ],
 };
 
 export default nextConfig;
